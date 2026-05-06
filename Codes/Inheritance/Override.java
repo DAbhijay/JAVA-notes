55408f7c0916dd085c34fa9e59cfccc2e916b4ba
DAbhijay <d.abhijay0808@gmail.com>
2026-05-06 10:57:05 +0530

    Override

---
+++ Codes/Inheritance/Override.java
@@ -0,0 +1,29 @@
+class Parent {
+    void show() {
+        System.out.println("Parent class");
+    }
+
+    void print(int x){
+        System.out.println("Value of x = " + x);
+    }
+}
+
+class Child extends Parent {
+    void show() {
+        System.out.println("Child class");
+    }
+
+    void print(int x, int y){
+        System.out.println("Value of double of x = " + 2 * x);
+        System.out.println("Value of y = " + y);
+    }
+}
+
+public class Override {
+    public static void main(String[] args) {
+        System.out.println("method overriding");
+
+        Parent p = new Child();
+        p.show();
+    }
+}
